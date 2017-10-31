80f2526c199b2dbe5a1841d4b4cda953364eb911
wishnuSepulsa <wishnu@sepulsa.com>
2017-11-01 05:11:06 +0700

    Use custom plugin layout in standalone layout

--- src/standalone/index.js
+++ src/standalone/index.js
@@ -1,12 +1,14 @@
 import StandaloneLayout from "./layout"
 import TopbarPlugin from "plugins/topbar"
 import ConfigsPlugin from "plugins/configs"
+import BaseLayoutPlugin from "plugins/slate"
 
 // the Standalone preset
 
 let preset = [
-  TopbarPlugin,
+  //TopbarPlugin,
   ConfigsPlugin,
+  BaseLayoutPlugin,
   () => {
     return {
       components: { StandaloneLayout }
